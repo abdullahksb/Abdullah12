cebe15e2d1c2551d6931d00fefa59babd09cb38b
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2025-05-12 13:09:11 +0530

    Initial commit

--- tests/Apitest.spec.js
+++ tests/Apitest.spec.js
@@ -1,15 +1,52 @@
-import { test,expect } from '@playwright/test';
+const {test , expect} = require('@playwright/test');
 
-test("API test", async ({ request }) => {
-  const response = await request.get("https://restful-booker.herokuapp.com/Booking");
-  console.log(await response.json());
-});
+let userid;
 
-test.only("API test 1", async () => {
-  const apiContext = await test.request.newContext({
-    baseURL: "https://restful-booker.herokuapp.com",
-  });
-  const response = await apiContext.get("/Booking");
-  const data = await response.json();
-  console.log(data);
+test('Get Users', async({request}) =>{
+
+const response =  await request.get('https://reqres.in/api/users?page=2')
+console.log(await response.json())
+expect(response.status()).toBe(200);
+})
+
+test('Create Users', async({request}) =>{
+
+    const response =  await request.post('https://reqres.in/api/users',
+        {
+            data:{"name": "kumar", "job": "trainer"},
+            headers:{"Content-Type": "application/json",
+                      "x-api-key": "reqres-free-v1"
+            },
+        } );
+        console.log(await response.json())
+        expect(response.status()).toBe(201)
+
+        var res = await response.json()
+        userid = res.id
+   
+})
+
+test('Update Users', async({request}) =>{
+    const response =  await request.put('https://reqres.in/api/users/'+userid,
+        {
+            data:{"name": "kumar", "job": "Engineer"},
+            headers:{"Content-Type": "application/json",
+                      "x-api-key": "reqres-free-v1"
+            },
+        } );
+        console.log(await response.json())
+        expect(response.status()).toBe(200)
+    })
+
+
+test('Delete Users', async({request}) =>{
+    const response =  await request.delete('https://reqres.in/api/users/'+userid,
+       
+            {
+                data:{"name": "kumar", "job": "Engineer"},
+                headers:{"Content-Type": "application/json",
+                          "x-api-key": "reqres-free-v1"
+                },
+            } )
+        expect(response.status()).toBe(204)
 });
